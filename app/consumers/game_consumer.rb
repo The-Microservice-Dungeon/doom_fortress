b93d5510b4ed55d3a5336e5f79c0d4ebefd52e3e
tsteffe <tristan.steffen@smail.th-koeln.de>
2022-01-20 12:15:31 +0100

    create Event object

--- app/consumers/game_consumer.rb
+++ app/consumers/game_consumer.rb
@@ -2,6 +2,9 @@ class GameConsumer < Racecar::Consumer
   subscribes_to "roundStatus"
 
   def process(message)
+    target = Event.new({event_id: message.headers["eventId"],topic: message.topic,timestamp: message.headers["timestamp"],event_type: message.headers["type"],value: message.value.to_s})
+    byebug
+    target.save
     puts "Received message: #{message.value}"
   end
 end
