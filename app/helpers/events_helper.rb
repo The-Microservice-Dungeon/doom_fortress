e7b9414c32bb1080ddf03beb2b10e4bc5b3231c9
tsteffe <tristan.steffen@smail.th-koeln.de>
2022-01-24 13:10:35 +0100

    added events_helper.rb

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,22 @@
 module EventsHelper
+  def getTemplate(topic)
+    case topic
+    when"fighting"
+      return {action: "fighting", target: "robot"}
+    when"mining"
+      return {action: "fighting", target: "robot"}
+    when"moving"
+      return {action: "fighting", target: "robot"}
+    when"buying"
+      return {action: "fighting", target: "robot"}
+    when"regeneration"
+      return {action: "fighting", target: "robot"}
+    when"blocking"
+      return {action: "fighting", target: "robot"}
+    when"destroyed"
+      return {action: "fighting", target: "robot"}
+    else
+      return {action: "fighting", target: "robot"}
+    end
+  end
 end
