3f369e9ddbc454226d76bbd3d05ce3084ce391fe
Your Name <your-mail@example.com>
2022-01-24 13:44:13 +0100

    actions to match given templates

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,22 +1,24 @@
+
 module EventsHelper
   def getTemplate(topic)
     case topic
+    when"mining"
+
+      return {action: "mining", target: "value"}
     when"fighting"
       return {action: "fighting", target: "robot"}
-    when"mining"
-      return {action: "fighting", target: "robot"}
     when"moving"
-      return {action: "fighting", target: "robot"}
+      return {action: "moving", target: "robot"}
     when"buying"
-      return {action: "fighting", target: "robot"}
+      return {action: "buying", target: "robot"}
     when"regeneration"
-      return {action: "fighting", target: "robot"}
+      return {action: "regeneration", target: "robot"}
     when"blocking"
-      return {action: "fighting", target: "robot"}
+      return {action: "blocking", target: "robot"}
     when"destroyed"
-      return {action: "fighting", target: "robot"}
+      return {action: "destroyed", target: "robot"}
     else
-      return {action: "fighting", target: "robot"}
+      return {action: "unknown", target: "robot"}
     end
   end
 end
