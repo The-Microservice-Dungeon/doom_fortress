e2de7cb2c722f28474163f0ed7854c48712202d2
Naski <jonas.marowski@smail.th-koeln.de>
2022-01-25 11:36:43 +0100

    subscribing to more events

--- app/consumers/game_consumer.rb
+++ app/consumers/game_consumer.rb
@@ -1,9 +1,16 @@
 class GameConsumer < Racecar::Consumer
-  subscribes_to "roundStatus"
+  subscribes_to "status"
+  subscribes_to "movement"
+  subscribes_to "mining"
+  subscribes_to "fighting"
+  subscribes_to "regeneration"
+  subscribes_to "trades"
+  subscribes_to "robot-destroyed"
+
+
 
   def process(message)
     target = Event.new({event_id: message.headers["eventId"],topic: message.topic,timestamp: message.headers["timestamp"],event_type: message.headers["type"],value: message.value.to_s})
-    byebug
     target.save
     puts "Received message: #{message.value}"
   end
